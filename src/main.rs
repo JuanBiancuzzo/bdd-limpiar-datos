bdc1d4fb6dfe789aa87582731390aa29e4294735
claram97 <cruano@fi.uba.ar>
2024-06-07 18:54:09 -0300

    arreglo formato de clean reviews, agrego logs del script

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 mod review;
 
+use chrono::Local;
 use serde_json::Value;
 use std::env;
 use std::fs::{self, File};
 use std::io::{BufRead, BufReader, Write};
 use std::process::exit;
+use std::fs::OpenOptions;
 
 const OUTPUT_FILE : &str = "datos/clean_reviews.csv";
+const PROGRAM_DATA : &str = "datos/program_data.txt";
+
+
+fn get_program_data_file(file_path : String) -> File {
+    let file = OpenOptions::new()
+    .append(true)  
+    .create(true)
+    .open(file_path).expect("Error: Couldn't open the file.");
+    return file;
+}
 
 fn file_exists(path: &str) -> bool {
     fs::metadata(path).is_ok()
@@ -68,17 +80,20 @@ fn obtain_file_with_parameters(
 
 fn validate_arguments() {
     let arguments: Vec<String> = env::args().collect();
-    if arguments.len() != 3 {
+    if arguments.len() != 3 && arguments.len() != 4 {
         eprintln!("Error: Se necesitan ambas rutas a los archivos de datos y parámetros.");
         exit(1);
     }
 }
 
-fn get_path_from_arguments() -> (Option<String>, Option<String>) {
+fn get_path_from_arguments() -> (Option<String>, Option<String>, Option<String>) {
     let arguments: Vec<String> = env::args().collect();
-    let path_datos = Some(arguments[1].clone());
-    let path_parametros = Some(arguments[2].clone());
-    (path_datos, path_parametros)
+    
+    let path_datos = arguments.get(1).cloned();
+    let path_parametros = arguments.get(2).cloned();
+    let path_program_data = arguments.get(3).cloned();
+    
+    (path_datos, path_parametros, path_program_data)
 }
 
 fn get_header_and_separator(parameters : Value) -> (usize, String){
@@ -95,10 +110,17 @@ fn get_header_and_separator(parameters : Value) -> (usize, String){
     return (header, sep)
 }
 
-fn process_file(data_file : BufReader<File>, mut clean_reviews : File, header : usize, sep : String) {
+fn process_file(data_file : BufReader<File>, mut clean_reviews : File, mut program_data : File, header : usize, sep : String) {
+    let start_time = Local::now();
+    let start_time_str = start_time.to_string();
+    let message = "Inicio del procesamiento del archivo de datos: ".to_string() + &start_time_str;
+    writeln!(program_data, "{}", message).expect("Error al escribir en el archivo de datos del programa");
+
+    let mut contador_ok = 0;
+    let mut contador_error = 0;
     for (i, line) in data_file.lines().enumerate() {
         if i < header {
-            writeln!(clean_reviews, "{}", line.unwrap()).expect("Error al escribir la cabecera en el archivo");
+            writeln!(clean_reviews, "reviewId,userName,content,score,thumbsUpCount,date,appVersion").expect("Error al escribir la cabecera en el archivo");
             continue;
         }
     
@@ -111,28 +133,42 @@ fn process_file(data_file : BufReader<File>, mut clean_reviews : File, header : 
         };
         match review::Review::new(&line, &sep) {
             Ok(review) => {
+                contador_ok+=1;
                 writeln!(
                     clean_reviews,
-                    "{};{};{};{};{};{};{}",
+                    "\"{}\",\"{}\",\"{}\",\"{}\",\"{}\",\"{}\",\"{}\"",
                     review.id,
                     review.user_name,
                     review.content,
                     review.score,
                     review.thumbs_up,
-                    review.app_version,
-                    review.date
+                    review.date,
+                    review.app_version
                 ).expect("Error al escribir la reseña en el archivo");
             },
             Err(_error) => {
-
+                contador_error+=1;
             }
         }
     }
+    let end_time = Local::now();
+    let end_time_str = end_time.to_string();
+    writeln!(program_data, "Fin del procesamiento del archivo de datos: {}", end_time_str).expect("Error al escribir en el archivo de datos del programa");
+
+    let duration = end_time - start_time;
+    let seconds = duration.num_seconds();
+    let minutes = seconds as f64 / 60.0;
+
+    let duration_str = format!("Tiempo total de procesamiento: {} segundos ({:.2} minutos)", seconds, minutes);
+    writeln!(program_data, "{}", duration_str).expect("Error al escribir en el archivo de datos del programa");
+    writeln!(program_data, "Cantidad de líneas sin errores: {}", contador_ok).expect("Error al escribir en el archivo de datos del programa");
+    writeln!(program_data, "Cantidad de líneas con errores: {}", contador_error).expect("Error al escribir en el archivo de datos del programa");
+    writeln!(program_data, "--------------------------------------------------------------------------------------").expect("Error al escribir en el archivo de datos del programa");
 }
 
 fn main() {
     validate_arguments();
-    let (data_path, parameters_path) = get_path_from_arguments();
+    let (data_path, parameters_path, program_data_path) = get_path_from_arguments();
     let (data_file, parameters) = 
         match obtain_file_with_parameters(data_path, parameters_path) {
             Some((data_file, parameters)) => (BufReader::new(data_file), parameters),
@@ -146,8 +182,9 @@ fn main() {
     
     let (header, sep) = get_header_and_separator(parameters);
 
-    //Crear el archivo. Si existe, simplemente re-escribirlo supongo.
+    let program_data = get_program_data_file(program_data_path.unwrap_or(PROGRAM_DATA.to_string()));
+
     let clean_reviews = create_output_file();
     
-    process_file(data_file, clean_reviews, header, sep);
+    process_file(data_file, clean_reviews, program_data, header, sep);
 }
